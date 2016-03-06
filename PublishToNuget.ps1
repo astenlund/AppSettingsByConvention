05d9d4a2105a9c9840f82ef72eae2afe9171d3a2
Daniel Ignat <dignat@kth.se>
2016-03-06 13:04:31 +0100

    Added two versions of png. Updated nuspec. Create tag in git when packing nuget file

--- PublishToNuget.ps1
+++ PublishToNuget.ps1
@@ -1,7 +1,15 @@
-nuget pack ./AppSettingsByConvention/AppSettingsByConvention.csproj -IncludeReferencedProjects
+nuget pack ./AppSettingsByConvention/AppSettingsByConvention.csproj -IncludeReferencedProjects -Build -Symbols -Properties Configuration=Release
+
 [xml]$Nuspec = Get-Content -Path ./AppSettingsByConvention/AppSettingsByConvention.nuspec
 $PackageId = $Nuspec.package.metadata.id
 $PackageVersion = $Nuspec.package.metadata.version
+
 $PackageFilename = "$PackageId.$PackageVersion.nupkg"
-Write-Host "Pushing package $$PackageFilename"
+Write-Host "Pushing package`t`t$PackageFilename"
 nuget push "./$PackageFilename"
+	
+$SymbolsPackageFilename = "$PackageId.$PackageVersion.symbols.nupkg"
+Write-Host "Pushing symbols package`t$SymbolsPackageFilename"
+nuget push "./$SymbolsPackageFilename"
+	
+git tag -a $PackageVersion -m "Packaged and pushed $PackageVersion to nuget.org"
