ad3c9da2956ac0379f45f802bc23154b78f74d05
Daniel Ignat <dignat@kth.se>
2016-03-10 19:44:31 +0100

    Extra unit test

---
+++ AppSettingsByConventionTests/RuntimeImplementation/WhenImplementingInterfaceButProvidingClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AppSettingsByConvention.RuntimeInterfaceImplementation;
+using AppSettingsByConventionTests.ConfigurationTargets;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AppSettingsByConventionTests.RuntimeImplementation
+{
+    [TestFixture]
+    public class WhenImplementingInterfaceButProvidingClass
+    {
+        [Test]
+        public void ShouldGetException()
+        {
+            Action implementClassWithProperties = () => typeof (SampleConfiguration).ImplementClassWithProperties();
+
+            implementClassWithProperties.ShouldThrow<InvalidOperationException>();
+        }
+    }
+}
